8239b44115238182b20e97878e749bc4d14f34ce
Yurii Soroka <yurisoroka165@gmail.com>
2023-01-24 12:26:12 +0200

    task-08

--- js/task-07.js
+++ js/task-07.js
@@ -1,12 +1,3 @@
-// Завдання 7
-// Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input)
-// і змінює інлайн - стиль span#text, оновлюючи властивість font - size.
-// В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
-
-// <input id="font-size-control" type="range" min="16" max="96" />
-// <br />
-// <span id="text">Abracadabra!</span>
-
 const refs = {
     input: document.querySelector('#font-size-control'),
     span: document.querySelector('#text'),
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', onFormsubmit);
+
+function onFormsubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        mail,
+        password,
+    }
+
+    if (mail === '' | password === '') {
+        alert('Будь ласка заповінть всі необхідні поля форми!');
+    } else {
+        console.log(formData);
+        event.currentTarget.reset();
+    }
+};
