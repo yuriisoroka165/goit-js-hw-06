c04928acd1957fc10edf93054bbc8038561143ff
Yurii Soroka <yurisoroka165@gmail.com>
2023-01-24 15:47:39 +0200

    10

--- js/task-10.js
+++ js/task-10.js
@@ -2,7 +2,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 const inputNumberEl = document.querySelector('#controls > input');
 const createButtonEl = document.querySelector('button[data-create]');
 const destroyButtonEl = document.querySelector('button[data-destroy]');
