4af414973c60521b23c60ee34c00d56d1cdde2fd
Yurii Soroka <yurisoroka165@gmail.com>
2023-01-24 12:42:00 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const pageBody = document.querySelector('body');
+const textOfBodyColor = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+changeColorButton.addEventListener('click', onChangeColorButtonHandler);
+
+function onChangeColorButtonHandler(event) {
+  pageBody.style.backgroundColor = getRandomHexColor();
+  textOfBodyColor.textContent = pageBody.style.backgroundColor;
+}
