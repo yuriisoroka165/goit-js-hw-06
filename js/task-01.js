e80a7d2f34953e132fc0019d79475f8ea7e68dec
Yurii Soroka <yurisoroka165@gmail.com>
2023-02-15 19:30:44 +0200

    1

--- js/task-01.js
+++ js/task-01.js
@@ -7,5 +7,5 @@ console.log('');
 for (let category of listOfCategoriesEl.children) {
     console.log(`Category: ${category.children[0].textContent}`);
     console.log(`Elements: ${category.children[1].children.length}`);
-    console.log('');
+    console.log('\n');
 }
